2e02c5897c6fb5753141edc2e29211580f7a76a3
Wallace S. Pimenta <wallacemancciny@gmail.com>
2021-04-27 23:21:28 -0300

    metade modulo de angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.eventos = [
+      {
+          Tema: "Angular 11",
+          Local: "Belo Horizonte"
+        },
+        {
+          Tema: ".NET 5",
+          Local: "São Paulo"
+        },
+        {
+          Tema: "Angular e suas novidades",
+          Local: "Rio de Janeiro"
+        }
+      ];
   }
 
 }
